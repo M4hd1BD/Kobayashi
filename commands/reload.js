308a22bd539beb4d3e2e447b20b6d2c2e1ebbda1
AL Mahdi <virtualmahdi@gmail.com>
2021-03-17 13:20:15 +0600

    added reload commandd

---
+++ commands/reload.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+module.exports = {
+	name: 'reload',
+	description: 'Reloads a command',
+	execute(message, args) {
+		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+        const commandName = args[0].toLowerCase();
+        const command = message.client.commands.get(commandName)
+            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+        
+        const commandFolder = fs.readdirSync('./commands');
+        delete require.cache[require.resolve(`../commands/${command.name}.js`)];
+
+        try {
+            const newCommand = require(`../commands/${command.name}.js`);
+            message.client.commands.set(newCommand.name, newCommand);
+            message.channel.send(`Command \`${command.name}\` was reloaded!`);
+        } catch (error) {
+            console.error(error);
+            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+        }
+        
+
+	},
+};
