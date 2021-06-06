3d411ed731abd09e7af3c51872494961fc6c65aa
Mahdi <virtualmahdi@gmail.com>
2021-06-06 15:50:37 +0600

    added option to add a role when a user joins, and some beatufying

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-const { prefix } = require('./config.json');
-const fs = require('fs');
-const dotenv = require('dotenv');
-dotenv.config()
+const { prefix } = require("./config.json");
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 global.isRunning = false;
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-
-client.once('ready', () => {
-    client.user.setPresence({ activity: { type: 'PLAYING', name: 'with YOU' }, status: 'online' });
-	console.log('Ready!');
+client.once("ready", () => {
+  client.user.setPresence({
+    activity: { type: "PLAYING", name: "with YOU" },
+    status: "online",
+  });
+  console.log("Ready!");
 });
 
-client.on('message', message => {
+client.on("message", (message) => {
+  if (
+    !message.content.startsWith(prefix) ||
+    message.author.bot ||
+    message.channel.type == "dm"
+  )
+    return;
 
-    if (!message.content.startsWith(prefix) || message.author.bot || message.channel.type == 'dm') return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
+  if (!client.commands.has(commandName)) {
+    message.channel.send(`It doesn't seem to be something I'm familiar with!`);
+    return;
+  }
 
-    if (!client.commands.has(commandName)) {
-        message.channel.send(`It doesn't seem to be something I'm familiar with!`);
-        return;
+  const command = client.commands.get(commandName);
+
+  if (isRunning) {
+    return message.reply(
+      "There already a command running, please wait for it to get done. This might be used as a input for that running command"
+    );
+  }
+
+  if (command.permissions) {
+    const authorPerm = message.channel.permissionsFor(message.author);
+
+    if (!authorPerm || !authorPerm.has(command.permissions)) {
+      return message.reply("You're not allowed to do this");
     }
+  }
 
-    const command = client.commands.get(commandName);
+  try {
+    isRunning = true;
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
 
-    if(isRunning) {
-        return message.reply("There already a command running, please wait for it to get done. This might be used as a input for that running command");
-    }
-
-    if(command.permissions) {
-        const authorPerm = message.channel.permissionsFor(message.author);
-
-        if(!authorPerm || !authorPerm.has(command.permissions)) {
-            return message.reply("You're not allowed to do this");
-        }
-    }
-
-    try {
-        isRunning = true;
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
-    }
-
+client.on("guildMemberAdd", (member) => {
+  const guild = client.guilds.cache.get("YOUR_ROLE_ID_HERE");
+  const role = guild.roles.cache.find((role) => role.name === "Peasant");
+  member.roles.add(role);
 });
 
 client.login(process.env.botToken);
